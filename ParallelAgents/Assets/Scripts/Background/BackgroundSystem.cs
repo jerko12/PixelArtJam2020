11622823978d6e37718024be8e500d4bae3b2b08
jerko12 <61011416+jerko12@users.noreply.github.com>
2020-09-01 01:24:49 +0200

    Player Animations and background start

--- ParallelAgents/Assets/Scripts/Background/BackgroundSystem.cs
+++ ParallelAgents/Assets/Scripts/Background/BackgroundSystem.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class BackgroundSystem : MonoBehaviour
 {
+    public GameObject backLane;
+    public GameObject midLane;
+    public GameObject frontLane;
+    public GameObject sky;
+    public GameObject detail;
+
+    public float backLaneSpeed = 1;
+    public float midLaneSpeed = 1;
+    public float frontLaneSpeed = 1;
+    public float skyLaneSpeed = 1;
+    public float detailLaneSpeed = 1;
+
+    public float backLaneSpawnTime = 1;
+    public float midLaneSpawnTime = 1;
+    public float foreLaneSpawnTime = 1;
+    public float skyLaneSpawnTime = 1;
+    public float detailLaneSpawnTime = 1;
+
     public Backgrounds backgrounds;
+
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
     // Start is called before the first frame update
     void Start()
     {
-        
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
     }
 
     // Update is called once per frame
@@ -16,4 +36,6 @@ public class BackgroundSystem : MonoBehaviour
     {
         
     }
+
+
 }
---
+++ ParallelAgents/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator animator;
+
+    public RunningState run;
+    public JumpingState jump;
+    public JetpackingState jetpack;
+    public DeadState dead;
+
+    public enum state
+    {
+        run,
+        jump,
+        jetpack,
+        dead
+    }
+
+    public state currentState;
+
+    public void init()
+    {
+        currentState = state.run;
+    }
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("IsJumping", GameManager.game.input.jump);
+        //animator.SetBool("",GameManager.game.input.);
+    }
+}
